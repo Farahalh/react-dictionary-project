c67ed9b0a0b1611de0d13fa28795e783990ac2d6
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-06-13 16:53:48 +0200

    making sure bootstrap is installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+      <header className="App-header">Hello</header>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
     </div>
   );
 }
